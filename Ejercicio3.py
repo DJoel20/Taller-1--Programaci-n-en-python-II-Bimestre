e522af52d98290221e5c83a9a5762117c1112df6
Alexander Paillacho <168491029+Alexandor31@users.noreply.github.com>
2024-06-26 13:24:50 -0500

    Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,23 +1,24 @@
 import sys
 
-def interact_with_troll():
-    n = int(input()) 
-    current_sequence = ['0'] * n 
-    
-    while True:
-        query = 'Q ' + ' '.join(current_sequence)
-        print(query) 
-        sys.stdout.flush()  
-        
-        response = int(input().strip())
-        
-        if response == n:
-            answer = 'A ' + ' '.join(current_sequence)
-            print(answer)  
-            sys.stdout.flush()  
-            break
-        else:
-            for i in range(n):
-                current_sequence[i] = '1' if current_sequence[i] == '0' else '0'
+n = int(input().strip())
 
-interact_with_troll()
+sequence = [0] * n
+
+print("Q", " ".join(map(str, sequence)))
+sys.stdout.flush()
+correct_bits = int(input().strip())
+
+for i in range(n):
+    sequence[i] = 1
+    
+    print("Q", " ".join(map(str, sequence)))
+    sys.stdout.flush()
+    new_correct_bits = int(input().strip())
+    
+    if new_correct_bits <= correct_bits:
+        sequence[i] = 0
+    else:
+        correct_bits = new_correct_bits
+
+print("A", " ".join(map(str, sequence)))
+sys.stdout.flush()
